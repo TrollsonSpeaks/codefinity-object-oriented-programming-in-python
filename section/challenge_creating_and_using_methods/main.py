2fce7af70b64bce022fdb260c0c111ef7957d255
TrollsonSpeaks <>
2026-03-02 09:57:09 +0000

    Challenge: Creating and Using Methods

--- section/challenge_creating_and_using_methods/main.py
+++ section/challenge_creating_and_using_methods/main.py
@@ -3,6 +3,12 @@ class Rectangle:
         self.width = width
         self.height = height
 
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return 2 * (self.width + self.height)
+
 rect = Rectangle(4, 5)
 print(rect.area())
 print(rect.perimeter())
